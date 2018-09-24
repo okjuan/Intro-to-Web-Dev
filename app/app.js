c574eeb484c185b03e230797bdb15e80bf3fd3a9
Juan Carlos Gallegos <jcgallegdup@gmail.com>
2018-09-23 19:56:59 -0700

    Create hello world express app

--- app/app.js
+++ app/app.js
@@ -5,10 +5,16 @@ const PORT = 8080;
 
 // This defines a resource at "/" for "GET" on our API.
 app.get('/', getHandler);
+app.get('/example', getHtmlPageHandler);
 
 function getHandler(request, response) {
     response.send("Welcome!");
     console.log("We just got a GET request at '/' in our API!");
+}
+
+function getHtmlPageHandler(request, response) {
+    response.sendFile(__dirname + "/index.html");
+    console.log("We just got a GET request at '/example' in our API!");
 }
 
 // Tells the Express framework what port to use.
